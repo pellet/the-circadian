0c7eaa16045f31da78a30c2e85359a1d28b2c04e
Anatoli Belchykau <belchikov1996@gmail.com>
2022-10-29 23:09:26 +0300

    Added neurofeedback

--- test.py
+++ test.py
@@ -2,10 +2,16 @@ import FieldTrip
 import mne
 
 import extract
+import nightlight
+
+import numpy as np
+from time import sleep
 
 
 ch_names = ["Fp1", "Fp2", "C3", "C4", "P3", "P4", "O1", "O2"]
 sfreq = 250
+
+values = []
 
 def get_raw_signal(signal_df, sampling_freq, ch_names):
     ch_types = ["eeg"] * len(ch_names)
@@ -27,6 +33,9 @@ def get_raw_signal(signal_df, sampling_freq, ch_names):
 c = FieldTrip.Client()
 c.connect(hostname='127.0.0.1', port=1973)
 
+c_o = FieldTrip.Client()
+c_o.connect(hostname='127.0.0.1', port=1974)
+
 while True:
     h = c.getHeader()
 
@@ -37,11 +46,21 @@ while True:
     raw_array = c.getData([
         n_samples - length, 
         n_samples - 1
-    ]).T
+    ]).T[1:9] # For OpenBCI only
 
     # raw = get_raw_signal(raw_array, sfreq, ch_names)
     signal_data = raw_array, sfreq, ch_names
 
     power_spectrum = extract.get_power_relative(signal_data)
+    value = power_spectrum['C3']['theta']['relative']
 
-    # print(power_spectrum)
+    values.append(value)
+    values = values[-10:]
+    mean_value = np.mean(values)
+    mean_value = (mean_value - 0.06) / (0.12 - 0.06)
+
+    print("Changing to: ", 1 - mean_value)
+
+    nightlight.set_night_light_value(mean_value)
+
+    sleep(1)
