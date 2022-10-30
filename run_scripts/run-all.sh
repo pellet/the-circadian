9fda2236f466ef1bbe6155ab70768e26f6a46715
Ben Pettit <benpettit@digimulti.com>
2022-10-30 19:01:18 +1000

    use the same root working directory

--- run_scripts/run-all.sh
+++ run_scripts/run-all.sh
@@ -3,28 +3,27 @@ redis-server &
 SLEEP_TIME=5
 sleep $SLEEP_TIME
 
-INIFILES='../../template_inifiles'
-cd eegsynth/module
+INIFILES='template_inifiles'
+EEGSYNTH_MODULE='eegsynth/module'
 
 # Execute buffer module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 1st step)
-python buffer/buffer.py -i $INIFILES/buffer.ini &
+python $EEGSYNTH_MODULE/buffer/buffer.py -i $INIFILES/buffer.ini &
 sleep $SLEEP_TIME
 
 # Execute openbci module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial3.md)
-./openbci2ft/openbci2ft.sh -i $INIFILES/openbci2ft.ini &
+$EEGSYNTH_MODULE/openbci2ft/openbci2ft.sh -i $INIFILES/openbci2ft.ini &
 sleep $SLEEP_TIME
 
 # Execute plotter module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 3rd step)
-python plotsignal/plotsignal.py -i $INIFILES/plotsignal.ini &
+python $EEGSYNTH_MODULE/plotsignal/plotsignal.py -i $INIFILES/plotsignal.ini &
 sleep $SLEEP_TIME
 
 # Execute preprocessing module
-python preprocessing/preprocessing.py -i $INIFILES/preprocessing.ini &
+python $EEGSYNTH_MODULE/preprocessing/preprocessing.py -i $INIFILES/preprocessing.ini &
 sleep $SLEEP_TIME
 
 # Execute nightlight neurofeedback
-cd ../../src
-PYTHONPATH=$PYTHONPATH:../eegsynth/lib MNE_USE_NUMBA=false python ./test.py
+PYTHONPATH=$PYTHONPATH:eegsynth/lib MNE_USE_NUMBA=false python ./src/test.py
 sleep $SLEEP_TIME
 
 # Wait for processes to finish
