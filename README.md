f5f42180581393e76273b5bc31b52d94d1e1e572
Ben Pettit <benpettit@digimulti.com>
2022-10-30 15:51:23 +1000

    use run-all for startup

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 - Clone this repo
-- Use the create-env.sh or update-env.sh for setting up the conda/python environment
+- Use create-env.sh or update-env.sh in install_scripts/ for setting up the conda/python environment
 - Fetch eegsynch submodule
 - Execute eegsynth/bin/install.sh
 - Install redis-server
-- Execute buffer module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 1st step)
-- Execute openbci module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial3.md)
-- Execute plotter module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 3rd step)
-
+- Use run-all.sh in run_scripts/ to start all the modules/services
 
 *Mac users must run terminal via Rosetta.*
---
+++ run_scripts/run-all.sh
@@ -0,0 +1,29 @@
+cd ..
+redis-server &
+SLEEP_TIME=5
+sleep $SLEEP_TIME
+
+INIFILES='../../template_inifiles'
+cd eegsynth/module
+
+# Execute buffer module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 1st step)
+python buffer/buffer.py -i $INIFILES/buffer.ini &
+sleep $SLEEP_TIME
+
+# Execute openbci module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial3.md)
+./openbci2ft/openbci2ft.sh -i $INIFILES/openbci2ft.ini &
+sleep $SLEEP_TIME
+
+# Execute plotter module (https://github.com/eegsynth/eegsynth/blob/master/doc/tutorial1.md, 3rd step)
+python plotsignal/plotsignal.py -i $INIFILES/plotsignal.ini &
+sleep $SLEEP_TIME
+
+# Execute preprocessing module
+python preprocessing/preprocessing.py -i $INIFILES/preprocessing.ini &
+sleep $SLEEP_TIME
+
+# Execute nightlight test
+cd ../..
+PYTHONPATH=$PYTHONPATH:./eegsynth/lib MNE_USE_NUMBA=false
+python src/test.py &
+sleep $SLEEP_TIME
